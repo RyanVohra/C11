7220a701cd5b7f062fe92296ec485c8321ea14ea
RyanVohra <87702860+RyanVohra@users.noreply.github.com>
2021-07-22 16:29:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var sum =0
+function setup() {
+  createCanvas(400,400);
+for(i = 0;i < weight.length; i=i+1){
+sum=sum+weight[i]
+
+}
+console.log(sum);
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
